1a62ca003f714fa9c24a266443138ad13314e5a4
Bastien Gautier <contact@bastiengautier.com>
2026-02-19 16:01:03 +0100

    docs: add missing docstrings in device_info.py

--- custom_components/clouding/device_info.py
+++ custom_components/clouding/device_info.py
@@ -7,7 +7,14 @@ from .const import DOMAIN, MANUFACTURER_NAME, PORTAL_URL
 
 
 class CloudingDeviceInfo:  # pylint: disable=too-few-public-methods
-    """..."""
+    """Provide device information for a Clouding.io server entity.
+
+    Attributes:
+        _device_name: The integration instance name configured by the user.
+        _model_name: The server image/model name.
+        _server_name: The display name of the server.
+        _server_unique_id: The unique identifier of the server.
+    """
 
     _device_name: str
     _model_name: str
@@ -15,7 +22,14 @@ class CloudingDeviceInfo:  # pylint: disable=too-few-public-methods
     _server_unique_id: str
 
     def __init__(self, device_name: str, model_name: str, server_name: str, server_unique_id: str) -> None:
-        """..."""
+        """Initialize CloudingDeviceInfo.
+
+        Args:
+            device_name: The integration instance name configured by the user.
+            model_name: The server image/model name.
+            server_name: The display name of the server.
+            server_unique_id: The unique identifier of the server.
+        """
 
         self._device_name = device_name
         self._model_name = model_name
@@ -24,7 +38,11 @@ class CloudingDeviceInfo:  # pylint: disable=too-few-public-methods
 
     @property
     def device_info(self) -> DeviceInfo:
-        """Return the device information of the entity."""
+        """Return the device information of the entity.
+
+        Returns:
+            A DeviceInfo instance populated with Clouding.io server metadata.
+        """
 
         return DeviceInfo(
             identifiers={(DOMAIN, slugify(f"{self._device_name} {self._server_unique_id}"))},
